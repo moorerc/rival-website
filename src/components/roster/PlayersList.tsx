db5a70f988d02dda8b5e8f871d0fe7c0b56d6586
Becky Moore <bmoore@palantir.com>
2019-03-30 17:31:59 -0700

    mobile roster player list

---
+++ src/components/roster/PlayersList.tsx
@@ -0,0 +1,44 @@
+import { Card, Elevation } from "@blueprintjs/core";
+import * as React from "react";
+import { RosterList } from "src/data/RosterList";
+import RosterUserAvatar from "./RosterUserAvatar";
+import { Players, PLAYERS } from "src/data/Players";
+import * as _ from "lodash";
+
+import "../../styles/PlayersList.css";
+import { getDisplayNameForPlayer } from "../basic/Helpers";
+
+interface PlayersListProps {
+  rosterList: RosterList;
+}
+
+export default class PlayersList extends React.Component<PlayersListProps> {
+  render() {
+    const peopleList = this.props.rosterList.players.concat(
+      this.props.rosterList.coaches
+    );
+    return (
+      <div className="players-list">
+        {_.map(peopleList, (player: Players, key: any) => (
+          <Card
+            interactive={false}
+            elevation={Elevation.ONE}
+            className="players-list-card"
+          >
+            <RosterUserAvatar
+              playerId={player}
+              player={PLAYERS[player]}
+              key={key}
+              rosterId={this.props.rosterList.id}
+              //   onClick={() => this.selectPlayer(player)}
+              //   noColor={this.state.selectedPlayer !== player}
+            />
+            <div className="card-title">
+              {getDisplayNameForPlayer(PLAYERS[player])}
+            </div>
+          </Card>
+        ))}
+      </div>
+    );
+  }
+}
