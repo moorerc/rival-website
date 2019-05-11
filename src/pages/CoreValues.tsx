2df13264b8b670c314a500d0c63f40abc43f5d73
Becky Moore <bmoore@palantir.com>
2019-05-11 08:36:42 -0700

    core values tool web

--- src/pages/CoreValues.tsx
+++ src/pages/CoreValues.tsx
@@ -4,205 +4,270 @@ import "../styles/CoreValues.css";
 
 import { isMobile } from "react-device-detect";
 import * as classNames from "classnames";
+import { Slider } from "@blueprintjs/core";
 
-export default class CoreValues extends React.Component {
-  render() {
-    return (
-      <div
-        className={classNames("rival-website-page rival-home-page", {
-          "-mobile": isMobile
-        })}
-      >
-        some stuff
-      </div>
-    );
+export namespace CoreValues {
+  export interface State {
+    brainValue: number;
+    effortValue: number;
+    skillValue: number;
+    characterValue: number;
+    triangleSize: number;
   }
 }
 
-/*
-var canvas = null;
-var context = null;
+const CANVAS_WIDTH = 500;
+const CANVAS_HEIGHT = 500;
+const CANVAS_X = 0;
+const CANVAS_Y = 0;
 
-var canvasWidth = 500;
-var canvasHeight = 500;
-var canvasX = 0;
-var canvasY = 0;
-
-var triangleSize = 115;
-
-var canvasCenter = {
-  x: (canvasX + canvasWidth) / 2,
-  y: (canvasY + canvasHeight) / 2
+const CANVAS_CENETR = {
+  x: (CANVAS_X + CANVAS_WIDTH) / 2,
+  y: (CANVAS_Y + CANVAS_HEIGHT) / 2
 };
 
-console.log(canvasCenter);
+const INITIAL_CIRCLE_VAL = 10;
+const INITIAL_TRIANGLE_VAL = 50;
 
-var pointACenter = {
-  x: canvasCenter.x,
-  y:
-    canvasCenter.y -
-    Math.sqrt(
-      triangleSize * triangleSize - (triangleSize / 2) * (triangleSize / 2)
-    ) /
-      2
-};
-
-var pointBCenter = {
-  x: canvasCenter.x - triangleSize / 2,
-  y:
-    canvasCenter.y +
-    Math.sqrt(
-      triangleSize * triangleSize - (triangleSize / 2) * (triangleSize / 2)
-    ) /
-      2
-};
-
-var pointCCenter = {
-  x: canvasCenter.x + triangleSize / 2,
-  y:
-    canvasCenter.y +
-    Math.sqrt(
-      triangleSize * triangleSize - (triangleSize / 2) * (triangleSize / 2)
-    ) /
-      2
-};
-
-var brainVal = 10;
-var effortVal = 10;
-var skillVal = 10;
-
-window.onload = function() {
-  canvas = document.getElementById("canvas");
-  context = canvas.getContext("2d");
-
-  drawTriangle();
-  drawBrainCircle();
-  updateTriangleSize(50);
-};
-
-function drawTriangle() {
-  var triangle = new Path2D();
-  triangle.moveTo(pointACenter.x, pointACenter.y);
-  triangle.lineTo(pointCCenter.x, pointCCenter.y);
-  triangle.lineTo(pointBCenter.x, pointBCenter.y);
-  triangle.lineTo(pointACenter.x, pointACenter.y);
-  context.strokeStyle = "rgba(34, 93, 57, 1.0)";
-  context.stroke(triangle);
-}
-
-function updateBrainVal(newBrainVal) {
-  brainVal = newBrainVal;
-  drawBrainCircle();
-}
-
-function updateEffortVal(newEffortVal) {
-  effortVal = newEffortVal;
-  drawBrainCircle();
-}
-
-function updateSkillVal(newSkillVal) {
-  skillVal = newSkillVal;
-  drawBrainCircle();
-}
-
-function updateTriangleSize(newTriangleSize) {
-  console.log(newTriangleSize);
-
-  triangleSize = -1 * (newTriangleSize - 100) + 100;
-
-  pointACenter = {
-    x: canvasCenter.x,
-    y:
-      canvasCenter.y -
-      Math.sqrt(
-        triangleSize * triangleSize - (triangleSize / 2) * (triangleSize / 2)
-      ) /
-        2
+export default class CoreValues extends React.Component<CoreValues.State> {
+  state: CoreValues.State = {
+    brainValue: INITIAL_CIRCLE_VAL,
+    effortValue: INITIAL_CIRCLE_VAL,
+    skillValue: INITIAL_CIRCLE_VAL,
+    characterValue: INITIAL_TRIANGLE_VAL,
+    triangleSize: -1 * (INITIAL_TRIANGLE_VAL - 100) + 100
   };
 
-  pointBCenter = {
-    x: canvasCenter.x - triangleSize / 2,
-    y:
-      canvasCenter.y +
-      Math.sqrt(
-        triangleSize * triangleSize - (triangleSize / 2) * (triangleSize / 2)
-      ) /
-        2
+  private canvasRef = React.createRef<HTMLCanvasElement>();
+
+  public componentDidMount() {
+    this.updateTriangle();
+  }
+
+  render() {
+    const { brainValue, characterValue, skillValue, effortValue } = this.state;
+    return (
+      <div
+        className={classNames("rival-website-page rival-core-values-page", {
+          "-mobile": isMobile
+        })}
+      >
+        <img className="page-title" src="img/core-values/CoreValuesTitle.png" />
+        <canvas
+          ref={this.canvasRef}
+          id="values-canvas"
+          width="500"
+          height="500"
+        />
+        <div className="values-controls" id="inputSet">
+          {this.renderInputControl(
+            "Brain",
+            "img/core-values/icon_brain.png",
+            brainValue,
+            this.handleBrainValueChange
+          )}
+          {this.renderInputControl(
+            "Effort",
+            "img/core-values/icon_effort.png",
+            effortValue,
+            this.handleEffortValueChange
+          )}
+          {this.renderInputControl(
+            "Skill",
+            "img/core-values/icon_skill.png",
+            skillValue,
+            this.handleSkillValueChange
+          )}
+          {this.renderInputControl(
+            "Character",
+            "img/core-values/icon_character.png",
+            characterValue,
+            this.handleCharacterValueChange
+          )}
+        </div>
+      </div>
+    );
+  }
+
+  private renderInputControl = (
+    name: string,
+    icon: string,
+    value: number,
+    onChange: (num: number) => void
+  ) => {
+    return (
+      <div className="input-control">
+        <div className="input-icon">
+          <img src={icon} />
+        </div>
+        <div className="input-name" id="nameBrain">
+          {name}
+        </div>
+        <Slider
+          className={classNames("input-slider", name.toLowerCase())}
+          min={0}
+          max={100}
+          stepSize={1}
+          value={value}
+          labelRenderer={false}
+          onChange={onChange}
+        />
+        <div className="input-value">{value}</div>
+      </div>
+    );
   };
 
-  pointCCenter = {
-    x: canvasCenter.x + triangleSize / 2,
-    y:
-      canvasCenter.y +
-      Math.sqrt(
-        triangleSize * triangleSize - (triangleSize / 2) * (triangleSize / 2)
-      ) /
-        2
+  private handleBrainValueChange = (brainValue: number) => {
+    this.setState({ brainValue });
+    this.updateCircles();
   };
 
-  drawBrainCircle();
+  private handleEffortValueChange = (effortValue: number) => {
+    this.setState({ effortValue });
+    this.updateCircles();
+  };
+
+  private handleSkillValueChange = (skillValue: number) => {
+    this.setState({ skillValue });
+    this.updateCircles();
+  };
+
+  private handleCharacterValueChange = (characterValue: number) => {
+    this.setState({ characterValue });
+    this.updateTriangle();
+  };
+
+  private updateCircles = () => {
+    const canvas = this.canvasRef.current;
+    if (canvas) {
+      const context = canvas.getContext("2d");
+      if (context) {
+        this.drawCircles(context);
+      }
+    }
+  };
+
+  private updateTriangle = () => {
+    const canvas = this.canvasRef.current;
+    if (canvas) {
+      const context = canvas.getContext("2d");
+      if (context) {
+        this.updateTriangleSize(context);
+      }
+    }
+  };
+
+  private getPointACenter() {
+    const { triangleSize } = this.state;
+    return {
+      x: CANVAS_CENETR.x,
+      y:
+        CANVAS_CENETR.y -
+        Math.sqrt(
+          triangleSize * triangleSize - (triangleSize / 2) * (triangleSize / 2)
+        ) /
+          2
+    };
+  }
+
+  private getPointBCenter() {
+    const { triangleSize } = this.state;
+    return {
+      x: CANVAS_CENETR.x - triangleSize / 2,
+      y:
+        CANVAS_CENETR.y +
+        Math.sqrt(
+          triangleSize * triangleSize - (triangleSize / 2) * (triangleSize / 2)
+        ) /
+          2
+    };
+  }
+
+  private getPointCCenter() {
+    const { triangleSize } = this.state;
+    return {
+      x: CANVAS_CENETR.x + triangleSize / 2,
+      y:
+        CANVAS_CENETR.y +
+        Math.sqrt(
+          triangleSize * triangleSize - (triangleSize / 2) * (triangleSize / 2)
+        ) /
+          2
+    };
+  }
+
+  private drawTriangle(context: CanvasRenderingContext2D) {
+    let triangle = new Path2D();
+    const pointACenter = this.getPointACenter();
+    const pointBCenter = this.getPointBCenter();
+    const pointCCenter = this.getPointCCenter();
+    triangle.moveTo(pointACenter.x, pointACenter.y);
+    triangle.lineTo(pointCCenter.x, pointCCenter.y);
+    triangle.lineTo(pointBCenter.x, pointBCenter.y);
+    triangle.lineTo(pointACenter.x, pointACenter.y);
+    context.strokeStyle = "rgba(34, 93, 57, 1.0)";
+    context.stroke(triangle);
+  }
+
+  private updateTriangleSize(context: CanvasRenderingContext2D) {
+    const { characterValue } = this.state;
+    this.setState({ triangleSize: -1 * (characterValue - 100) + 100 });
+    this.drawCircles(context);
+  }
+
+  private drawCircles(context: CanvasRenderingContext2D) {
+    const { triangleSize, brainValue, effortValue, skillValue } = this.state;
+    context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    context.lineWidth = 2;
+    this.drawTriangle(context);
+    context.lineWidth = 1;
+    this.setState({ triangleVal: -1 * (triangleSize - 100) + 100 });
+
+    const pointACenter = this.getPointACenter();
+    const pointBCenter = this.getPointBCenter();
+    const pointCCenter = this.getPointCCenter();
+
+    let brainCircle = new Path2D();
+    context.fillStyle = "rgba(0, 61, 167, 0.3)";
+    context.strokeStyle = "rgba(0, 61, 167, 1.0)";
+    brainCircle.arc(
+      pointBCenter.x,
+      pointBCenter.y,
+      brainValue,
+      0,
+      Math.PI * 2,
+      true
+    );
+    context.fill(brainCircle);
+    context.stroke(brainCircle);
+
+    let effortCircle = new Path2D();
+    context.fillStyle = "rgba(181, 127, 0, 0.3)";
+    context.strokeStyle = "rgba(181, 127, 0, 1.0)";
+    effortCircle.arc(
+      pointACenter.x,
+      pointACenter.y,
+      effortValue,
+      0,
+      Math.PI * 2,
+      true
+    );
+    context.fill(effortCircle);
+    context.stroke(effortCircle);
+
+    let skillCircle = new Path2D();
+    context.fillStyle = "rgba(121, 36, 47, 0.3)";
+    context.strokeStyle = "rgba(121, 36, 47, 1.0)";
+    skillCircle.arc(
+      pointCCenter.x,
+      pointCCenter.y,
+      skillValue,
+      0,
+      Math.PI * 2,
+      true
+    );
+    context.fill(skillCircle);
+    context.stroke(skillCircle);
+  }
 }
-
-function drawBrainCircle() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-
-  context.lineWidth = 2;
-  drawTriangle();
-  context.lineWidth = 1;
-  document.getElementById("triangleVal").innerHTML =
-    -1 * (triangleSize - 100) + 100;
-
-  var brainCircle = new Path2D();
-  context.fillStyle = "rgba(0, 61, 167, 0.3)";
-  context.strokeStyle = "rgba(0, 61, 167, 1.0)";
-  brainCircle.arc(
-    pointBCenter.x,
-    pointBCenter.y,
-    brainVal,
-    0,
-    Math.PI * 2,
-    true
-  );
-  context.fill(brainCircle);
-  context.stroke(brainCircle);
-  document.getElementById("brainVal").innerHTML = brainVal;
-
-  var effortCircle = new Path2D();
-  context.fillStyle = "rgba(181, 127, 0, 0.3)";
-  context.strokeStyle = "rgba(181, 127, 0, 1.0)";
-  effortCircle.arc(
-    pointACenter.x,
-    pointACenter.y,
-    effortVal,
-    0,
-    Math.PI * 2,
-    true
-  );
-  context.fill(effortCircle);
-  context.stroke(effortCircle);
-  document.getElementById("effortVal").innerHTML = effortVal;
-
-  var skillCircle = new Path2D();
-  context.fillStyle = "rgba(121, 36, 47, 0.3)";
-  context.strokeStyle = "rgba(121, 36, 47, 1.0)";
-  skillCircle.arc(
-    pointCCenter.x,
-    pointCCenter.y,
-    skillVal,
-    0,
-    Math.PI * 2,
-    true
-  );
-  context.fill(skillCircle);
-  context.stroke(skillCircle);
-  document.getElementById("skillVal").innerHTML = skillVal;
-}
-
-function saveImage() {
-  var img = canvas.toDataURL("image/png");
-  document.write('<img src="' + img + '"/>');
-}
-
-
-
-*/
