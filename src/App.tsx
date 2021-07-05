ae1c8b0dc8dbb6aed53cc186ddfe402804495c90
Becky Moore <bmoore@palantir.com>
2021-07-05 11:57:49 -0400

    remove scott secret santa

--- src/App.tsx
+++ src/App.tsx
@@ -9,39 +9,26 @@ import { Results } from "./pages/Results";
 import { Roster } from "./pages/Roster";
 import { RosterNineteen } from "./pages/RosterNineteen";
 import "./styles/App.css";
-import classNames from "classnames";
 import CoreValues from "./pages/CoreValues";
-import { ScottSecretSanta } from "./pages/ScottSecretSanta"
 
 export default class App extends React.Component {
   render() {
-    // const isSidebarVisible = true;
-
     return (
-      <React.Fragment>
         <div
-          className={classNames("rival-website-app", {
-            // "-sidebar-visible": isSidebarVisible
-          })}
+          className="rival-website-app"
         >
           <BrowserRouter>
-            <React.Fragment>
               <Route exact={true} path="/" component={HomeOG} />
               <Route path="/about" component={About} />
               <Route path="/news" component={News} />
               <Route path="/results" component={Results} />
-              {/* <Route path="/roster" render={props => <Roster {...props} />} /> */}
               <Route path="/roster" component={Roster} />
               <Route path="/badgequest" component={BadgeQuest} />
               <Route path="/fiftyplates" component={FiftyPlates} />
               <Route path="/corevalues" component={CoreValues} />
               <Route path="/roster2019" component={RosterNineteen} />
-              <Route path="/sss2019" component={ScottSecretSanta} />
-            </React.Fragment>
           </BrowserRouter>
         </div>
-        {/* <div className="rival-app-page-sidebar"></div> */}
-      </React.Fragment>
     );
   }
 }
--- src/state/actions.tsx
+++ src/state/actions.tsx
@@ -1,13 +1,8 @@
 import { TypedAction } from "redoodle";
 import { RosterList } from "src/data/RosterList";
-import { SSSNavTab } from "src/data/sss/SSS";
 
 export const PREFIX = "@rival-website";
 
 export const SELECT_ROSTER = TypedAction.define(`${PREFIX}//SELECT_ROSTER`)<
   RosterList
->();
-
-export const SELECT_SSS_TAB = TypedAction.define(`${PREFIX}//SELECT_SSS_TAB`)<
-  SSSNavTab
 >();
